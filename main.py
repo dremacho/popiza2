bcd16284af44414c7599e70d1fd57d9a5df6b0e3
DreminYaroslav <144015390+dremacho@users.noreply.github.com>
2025-09-04 15:35:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from aiogram import Bot, Dispatcher
-import asyncio
-
-API_TOKEN = "YOUR_BOT_TOKEN"  # вставь свой токен телеграм-бота
-
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
-
-async def main():
-    print("Бот запущен")
-    await bot.session.close()  # закрываем сессию, чтобы не было ошибок
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print("Hello world")
